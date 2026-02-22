a6405d5517060c59419c8b408636cf04a549aea5
Cyenia <35280763+Cyenia@users.noreply.github.com>
2026-02-22 21:20:01 +0100

    Add helper method for displaying mount fights tooltip

---
+++ MountTracker/Classes/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using Dalamud.Bindings.ImGui;
+
+namespace MountTracker.Classes;
+
+public static class Helpers
+{
+    public static void DrawCurrentMountFightsTooltip(OrderedDictionary<uint, Classes.Mount> mounts, uint currentMountId)
+    {
+        if (mounts.TryGetValue(currentMountId, out var mountData))
+        {
+            if (ImGui.IsItemHovered())
+            {
+                var tooltip = string.Join("\r\n", mountData.Fights.Values);
+                ImGui.SetTooltip(tooltip);
+            }
+        }
+    }
+}
--- MountTracker/Services/PluginServices.cs
+++ MountTracker/Services/PluginServices.cs
@@ -11,4 +11,6 @@ public class PluginServices
     [PluginService] internal static IPartyList PartyList { get; private set; } = null!;
     [PluginService] internal static IPlayerState PlayerState { get; private set; } = null!;
     [PluginService] internal static IDataManager DataManager { get; private set; } = null!;
+    [PluginService] internal static IClientState ClientState { get; private set; } = null!;
+    
 }
